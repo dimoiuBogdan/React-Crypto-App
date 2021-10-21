21a2d87450233cac8112a9effbd220386583bda6
dimoiuBogdan <bogdybogdan34@gmail.com>
2021-10-21 20:16:50 +0300

    Implementing redux store

--- src/App.js
+++ src/App.js
@@ -39,8 +39,21 @@ const App = () => {
             </Switch>
           </div>
         </Layout>
+        <footer className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            Cryptoverse <br />
+            All rights reserved.
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </footer>
       </main>
-      <footer className="footer"></footer>
     </div>
   );
 };
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,8 @@
+var options = {
+  method: "GET",
+  url: "https://coinranking1.p.rapidapi.com/stats",
+  headers: {
+    "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+    "x-rapidapi-key": "3590d43b63msh102f766e560b92dp1833abjsn114424f52068",
+  },
+};
